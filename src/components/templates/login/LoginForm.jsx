740d282c8ec8b053a51484037ea8fa774d68bf43
Alireza Ghanbari <91381302+alirezza96@users.noreply.github.com>
2024-09-16 14:47:52 +0330

    upgrade react, react-dom, next because i want to use useActionState

--- src/components/templates/login/LoginForm.jsx
+++ src/components/templates/login/LoginForm.jsx
@@ -2,13 +2,13 @@
 import Input from "@/components/modules/forms/Input"
 import Submit from "@/components/modules/forms/Submit"
 import { login } from "@/utils/auth/actions"
-// import { useActionState } from "react"
+import { useFormState } from "react"
 export default function LoginForm() {
     const initialState = { message: null, errors: null }
-    // const [formState, formAction, isPending] = useActionState(
-    //     login,
-    //     initialState
-    // )
+    const [formState, formAction] = useFormState(
+        login,
+        initialState
+    )
     return (
         <form action={login}>
             <Input
